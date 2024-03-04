8ea3dbaea586755da812c5819c24112cf0eb0823
trediptagw <tredipta.mondal@greenwave.co.in>
2024-03-04 17:07:00 +0530

    made changes to textfield and app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React, { useState } from "react";
 import "./App.css";
+import Textfield from "./components/textfield/textfield.component";
 
 function App() {
+  // const handleTextfieldChange = (event) => {
+  //   setValue(event.target.value);
+  // };
+
   return (
     <div className="App">
       <h1>qwerty</h1>
+      <Textfield id={"textfieldId"} value={"hello there"} />
     </div>
   );
 }
--- src/components/textfield/textfield.component.jsx
+++ src/components/textfield/textfield.component.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { TextField } from '@mui/material'
+import React from 'react';
+import { TextField } from '@mui/material';
 
-export default function textfield() {
+export default function Textfield({ id, value, onChange }) {
   return (
     <>
-      <TextField/>
+      <TextField id={id} value={value} onChange={onChange} />
     </>
-  )
+  );
 }

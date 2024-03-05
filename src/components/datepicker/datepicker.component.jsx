73970ae2d14cb25d6b6b9d9d4180a350cf4803c2
trediptagw <tredipta.mondal@greenwave.co.in>
2024-03-05 14:40:33 +0530

    update Datepicker

--- src/components/datepicker/datepicker.component.jsx
+++ src/components/datepicker/datepicker.component.jsx
@@ -2,16 +2,10 @@ import React from "react";
 import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-export default function DatePicker() {
+export default function Datepicker({ label }, { value }, { onChange }) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker
-        label="Support Start Date"
-        value={formData.supportStartDate}
-        onChange={(startDate) =>
-          setFormData({ ...formData, supportStartDate: startDate })
-        }
-      />
+      <DatePicker label={label} value={value} onChange={onChange} />
     </LocalizationProvider>
   );
 }

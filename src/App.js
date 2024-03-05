2fac98ea3f9869fd4c1a35c04a6bfbc80f9e2462
trediptagw <tredipta.mondal@greenwave.co.in>
2024-03-05 16:40:42 +0530

    added some pages and updated app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Textfield from "./components/textfield/textfield.component";
 import Device from "./modules/device/modules.device";
 import NavigationArea from "./components/navigationbar/navigationbar.component";
+import AdminConfigurationHome from "./components/AdminConfigurationHome";
+import UserRegistration from "./components/UserRegistration";
 
 function App() {
-  const urllist=[
+  const urllist = [
     { pageName: "Test", pagelink: "/Test" },
-    { pageName: "IssueCategory", pagelink: "/IssueCategory" }
-  ]
-  const onclick=()=>{
-    console.log("Hamburger Click")
-  }
+    { pageName: "IssueCategory", pagelink: "/IssueCategory" },
+  ];
+  const onclick = () => {
+    console.log("Hamburger Click");
+  };
   return (
-    <div className="App">
-      <NavigationArea urllist={urllist} handleOnClick={onclick}/>
-      <h1>qwerty</h1>
-      <Textfield id={"textfieldId"} value={"hello there"} />
-      <Device/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavigationArea urllist={urllist} handleOnClick={onclick} />
+        <h1>qwerty</h1>
+        <Textfield id={"textfieldId"} value={"hello there"} />
+        <Device />
+        <Routes>
+          <Route path="/ad" element={<AdminConfigurationHome />} />
+          <Route path="/UserRegistration" element={<UserRegistration />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

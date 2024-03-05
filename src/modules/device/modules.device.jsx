801b80e17d10985b660d11bd1adf2af5941af57b
sreepriyaroy <sreepriya.roy@greenwave.co.in>
2024-03-05 12:21:34 +0530

    test

--- src/modules/device/modules.device.jsx
+++ src/modules/device/modules.device.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Dropdown from '../../components/dropdown/dropdown.component'
 export default function Device() {
     const list=[1,2,3,4]

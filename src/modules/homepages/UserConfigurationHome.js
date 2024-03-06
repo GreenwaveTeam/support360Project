78bad5ad96be03bfa5c0c3bd8f0dfebd5501a21f
trediptagw <tredipta.mondal@greenwave.co.in>
2024-03-06 14:10:51 +0530

    .

--- src/modules/homepages/UserConfigurationHome.js
+++ src/modules/homepages/UserConfigurationHome.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Button, Grid } from "@mui/material";
 import { Link } from "react-router-dom";
-import { CenterFocusStrong } from "@mui/icons-material";
 
 export default function UserConfigurationHome() {
   const [list, setList] = useState([]);

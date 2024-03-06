e3810266b627d761f3e0a576e147f382f1737845
trediptagw <tredipta.mondal@greenwave.co.in>
2024-03-06 12:04:40 +0530

    ...

--- src/modules/homepages/UserConfigurationHome.js
+++ src/modules/homepages/UserConfigurationHome.js
@@ -29,7 +29,6 @@ export default function UserConfigurationHome() {
     <>
       <Button
         type="submit"
-        fullWidth
         variant="contained"
         sx={{ mt: 3, mb: 2 }}
         href="/UserRegistration"

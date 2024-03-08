5a802faae9414d4c0aae0f258169eee53baadee8
trediptagw <tredipta.mondal@greenwave.co.in>
2024-03-08 11:10:38 +0530

    update homepage

--- src/modules/homepages/UserConfigurationHome.js
+++ src/modules/homepages/UserConfigurationHome.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Button, Grid } from "@mui/material";
+import {
+  Button,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 
 export default function UserConfigurationHome() {
@@ -19,6 +28,10 @@ export default function UserConfigurationHome() {
       });
       const data = await response.json();
       setList(data);
+      console.log(data);
+      for (let i of data) {
+        console.log(i.userID, i.email, i.name);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -36,30 +49,45 @@ export default function UserConfigurationHome() {
       </Button>
       <Grid item xs={12} justifyContent={"center"}>
         <h3>Existing Users</h3>
-        {list.map((item, index) => (
-          <Link
-            // to={`http://localhost:8081/users/user/${item.userID}`}
-            to={`http://localhost:3000/abc`}
-            style={{ textDecoration: "none", color: "inherit" }}
-            key={index}
-          >
-            <div
-              style={{
-                border: "2px solid grey",
-                borderRadius: "20px",
-                color: "inherit",
-                listStyleType: "none",
-                marginBottom: "20px",
-                padding: "7px",
-                backgroundColor: "lightgray",
-                alignItems: "center",
-              }}
-              key={index}
-            >
-              {item.name}
-            </div>
-          </Link>
-        ))}
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>User ID</TableCell>
+                <TableCell>Edit</TableCell>
+                <TableCell>Delete</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {list.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell>
+                    <Link
+                      to={`http://localhost:3000/abc`}
+                      style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                      {item.name}
+                    </Link>
+                  </TableCell>
+                  <TableCell>{item.email}</TableCell>
+                  <TableCell>{item.userID}</TableCell>
+                  <TableCell>
+                    <Button variant="outlined" color="primary">
+                      Edit
+                    </Button>
+                  </TableCell>
+                  <TableCell>
+                    <Button variant="outlined" color="secondary">
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
     </>
   );
--- src/modules/registration/UserRegistration.js
+++ src/modules/registration/UserRegistration.js
@@ -31,36 +31,47 @@ export default function UserRegistration() {
     plantID: "",
     plantName: "",
     address: "",
-    business: "",
+    division: "",
     customerName: "",
     supportStartDate: "",
     supportEndDate: "",
     accountOwnerCustomer: "",
     accountOwnerGW: "",
   });
+  const [newPlantName, setNewPlantName] = useState({
+    plantName: "",
+    plantID: "",
+  });
   const [cnfpass, setCnfpass] = useState("");
-  const [showError, setShowError] = useState(false);
+  const [pass, setPass] = useState("");
+  const [showPasswordError, setShowPasswordError] = useState(false);
+  const [showDateError, setShowDateError] = useState(false);
   const [plantList, setPlantList] = useState([]);
+  const [showPlantTextField, setShowPlantTextField] = useState(false);
+  const [hideBtn, setHideBtn] = useState(false);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [newPlantName.plantName]);
 
-  const handleInputChange = (event) => {
+  const handleAddPlantClick = () => {
+    setShowPlantTextField(true);
+    setHideBtn(true);
+  };
+
+  const hashedPasswordChange = (e) => {
+    setPass(e.target.value);
+  };
+
+  const handleFormdataInputChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const generateUserID = () => {
-    if (formData.plantName && formData.name) {
-      const plantFirstWord = formData.plantName.split(" ")[0].toLowerCase;
-      const userFirstName = formData.name.split(" ")[0].toLowerCase;
-      const userID = `${plantFirstWord}_${userFirstName}`;
-      setFormData((prevData) => ({
-        ...prevData,
-        userID: userID.toLowerCase(),
-      }));
-    }
+  const handlenewPlantNameInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewPlantName({ ...newPlantName, [name]: value });
+    console.log(name, value);
   };
 
   const handleDesignationChange = (event) => {
@@ -75,8 +86,17 @@ export default function UserRegistration() {
     }
   };
 
-  const checkPassword = async () => {
-    return await bcrypt.compare(formData.password, cnfpass);
+  const confirmPassword = async (e) => {
+    const confirmPass = e.target.value;
+    setCnfpass(confirmPass);
+    const hashedPassword = await bcrypt.hash(pass, 10);
+    setFormData({ ...formData, password: hashedPassword });
+    const passwordsMatch = await bcrypt.compare(confirmPass, hashedPassword);
+    if (!passwordsMatch) {
+      setShowPasswordError(true);
+    } else {
+      setShowPasswordError(false);
+    }
   };
 
   const fetchData = async () => {
@@ -94,45 +114,50 @@ export default function UserRegistration() {
     }
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const passwordsMatch = await checkPassword();
-
-    if (!passwordsMatch) {
-      setShowError(true);
-      setTimeout(() => {
-        setShowError(false);
-      }, 2000);
-      return;
-    }
-
-    const hashedPassword = await bcrypt.hash(formData.password, 10);
-    const formDataWithChangedPassword = {
-      ...formData,
-      password: hashedPassword,
-    };
-
+  const postPlantName = async () => {
     try {
-      const response = await fetch("http://localhost:8081/users/user", {
+      const response = await fetch("http://localhost:8081/plants/plant", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formDataWithChangedPassword),
+        body: JSON.stringify(newPlantName),
       });
       if (response.created) {
-        console.log("User registered successfully");
+        console.log("Plant Added successfully");
       } else {
-        console.error("Failed to register user");
+        console.error("Failed to Add Plant");
       }
-      console.log(
-        "formDataWithChangedPassword : ",
-        formDataWithChangedPassword
-      );
+      console.log("newPlantName : ", newPlantName);
     } catch (error) {
       console.error("Error:", error);
     }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // try {
+    //   const response = await fetch("http://localhost:8081/users/user", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(formDataWithChangedPassword),
+    //   });
+    //   if (response.created) {
+    //     console.log("User registered successfully");
+    //   } else {
+    //     console.error("Failed to register user");
+    //   }
+    //   console.log(
+    //     "formDataWithChangedPassword : ",
+    //     formDataWithChangedPassword
+    //   );
+    // } catch (error) {
+    //   console.error("Error:", error);
+    // }
+    console.log("formData : ", formData);
   };
 
   return (
@@ -165,7 +190,19 @@ export default function UserRegistration() {
                   label="Name"
                   autoFocus
                   value={formData.name}
-                  onChange={handleInputChange}
+                  onChange={handleFormdataInputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="userID"
+                  name="userID"
+                  required
+                  fullWidth
+                  id="userID"
+                  label="User ID"
+                  value={formData.userID}
+                  onChange={handleFormdataInputChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -190,11 +227,12 @@ export default function UserRegistration() {
                   required
                   fullWidth
                   id="email"
+                  type="email"
                   label="Email Address"
                   name="email"
                   autoComplete="email"
                   value={formData.email}
-                  onChange={handleInputChange}
+                  onChange={handleFormdataInputChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -203,11 +241,11 @@ export default function UserRegistration() {
                   fullWidth
                   name="password"
                   label="Password"
-                  type="password"
+                  // type="password"
                   id="password"
                   // autoComplete="new-password"
-                  value={formData.password}
-                  onChange={handleInputChange}
+                  value={pass}
+                  onChange={hashedPasswordChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -216,12 +254,22 @@ export default function UserRegistration() {
                   fullWidth
                   name="confirmPassword"
                   label="Confirm Password"
-                  type="password"
+                  // type="password"
                   id="confirmPassword"
                   value={cnfpass}
-                  onChange={(e) => setCnfpass(e.target.value)}
+                  onChange={(e) => confirmPassword(e)}
                 />
               </Grid>
+              {showPasswordError && (
+                <Stack
+                  sx={{ display: "flex", justifyContent: "right" }}
+                  spacing={2}
+                >
+                  <Alert variant="filled" severity="error">
+                    Password Does Not Match
+                  </Alert>
+                </Stack>
+              )}
               <Grid item xs={12}>
                 <TextField
                   required
@@ -234,54 +282,116 @@ export default function UserRegistration() {
                   onChange={handlePhoneNumberChange}
                 />
               </Grid>
-              {/* <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="plantName"
-                  label="Plant Name"
-                  id="plantName"
-                  autoComplete="plantName"
-                  value={formData.plantName}
-                  onChange={handleInputChange}
-                />
-              </Grid> */}
               <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <InputLabel>Plant Name</InputLabel>
-                  <Select
-                    required
-                    id="plantName"
-                    value={formData.plantName}
-                    label="Plant Name"
-                    onChange={(event) => {
-                      const { value } = event.target;
-                      for (let i of plantList) {
-                        if (i.plantName === value) {
-                          setFormData({
-                            ...formData,
-                            plantID: i.plantID,
-                            plantName: value,
-                          });
-                          console.log(
-                            "plantName : ",
-                            value,
-                            " : plantID : ",
-                            i.plantID
-                          );
-                          return;
-                        }
-                      }
-                    }}
-                    name="plantName"
-                  >
-                    {plantList.map((p) => (
-                      <MenuItem key={p.plantID} value={p.plantName}>
-                        {p.plantName}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
+                {showPlantTextField ? (
+                  <>
+                    <TextField
+                      required
+                      style={{ width: "40%" }}
+                      name="plantName"
+                      label="Plant Name"
+                      id="plantName"
+                      value={newPlantName.plantName}
+                      onChange={handlenewPlantNameInputChange}
+                    />
+                    <TextField
+                      required
+                      style={{ width: "40%" }}
+                      name="plantID"
+                      label="PlantID"
+                      id="plantID"
+                      value={newPlantName.plantID}
+                      onChange={handlenewPlantNameInputChange}
+                    />
+                    <Button
+                      id="save"
+                      onClick={(e) => {
+                        postPlantName();
+                        setShowPlantTextField(false);
+                        setHideBtn(false);
+                        fetchData();
+                      }}
+                      style={{
+                        width: "10%",
+                        height: "56px",
+                        borderRadius: "50px",
+                      }}
+                      variant="contained"
+                      color="success"
+                    >
+                      Save
+                    </Button>
+                    <Button
+                      id="cancel"
+                      onClick={(e) => {
+                        setShowPlantTextField(false);
+                        setHideBtn(false);
+                      }}
+                      style={{
+                        width: "10%",
+                        height: "56px",
+                        borderRadius: "50px",
+                      }}
+                      variant="contained"
+                      color="error"
+                    >
+                      Cancel
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <FormControl style={{ width: "40%" }}>
+                      <InputLabel>Plant Name</InputLabel>
+                      <Select
+                        required
+                        id="plantName"
+                        value={formData.plantName}
+                        label="Plant Name"
+                        onChange={(event) => {
+                          const { value } = event.target;
+                          for (let i of plantList) {
+                            if (i.plantName === value) {
+                              setFormData({
+                                ...formData,
+                                plantID: i.plantID,
+                                plantName: value,
+                              });
+                              return;
+                            }
+                          }
+                        }}
+                        name="plantName"
+                      >
+                        {plantList.map((p) => (
+                          <MenuItem key={p.plantID} value={p.plantName}>
+                            {p.plantName}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                    <TextField
+                      required
+                      style={{ width: "40%" }}
+                      name="plantID"
+                      label="PlantID"
+                      id="plantID"
+                      value={formData.plantID}
+                    />
+                    <Button
+                      id="addPlantName"
+                      onClick={handleAddPlantClick}
+                      style={{
+                        width: "20%",
+                        height: "56px",
+                        display: hideBtn ? "none" : "inline",
+                        borderRadius: "50px",
+                      }}
+                      variant="contained"
+                    >
+                      Add New Plant
+                    </Button>
+                  </>
+                )}
               </Grid>
               <Grid item xs={12}>
                 <TextField
@@ -292,19 +402,19 @@ export default function UserRegistration() {
                   id="address"
                   autoComplete="address"
                   value={formData.address}
-                  onChange={handleInputChange}
+                  onChange={handleFormdataInputChange}
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
-                  name="business"
-                  label="Business"
-                  id="business"
-                  autoComplete="business"
-                  value={formData.business}
-                  onChange={handleInputChange}
+                  name="division"
+                  label="Division"
+                  id="division"
+                  autoComplete="division"
+                  value={formData.division}
+                  onChange={handleFormdataInputChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -316,7 +426,7 @@ export default function UserRegistration() {
                   id="customerName"
                   autoComplete="customerName"
                   value={formData.customerName}
-                  onChange={handleInputChange}
+                  onChange={handleFormdataInputChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -337,14 +447,29 @@ export default function UserRegistration() {
                   <DatePicker
                     label="Support End Date"
                     value={formData.supportEndDate}
-                    onChange={(endDate) =>
-                      setFormData({ ...formData, supportEndDate: endDate })
-                    }
+                    onChange={(endDate) => {
+                      if (endDate < formData.supportEndDate) {
+                        setShowDateError(true);
+                      } else {
+                        setShowDateError(false);
+                        setFormData({ ...formData, supportEndDate: endDate });
+                      }
+                    }}
                     format="DD/MM/YYYY"
                     slotProps={{ textField: { fullWidth: true } }}
                   />
                 </LocalizationProvider>
               </Grid>
+              {showDateError && (
+                <Stack
+                  sx={{ display: "flex", justifyContent: "right" }}
+                  spacing={2}
+                >
+                  <Alert variant="filled" severity="error">
+                    SupportEndDate Should Not be less than SupportStartDate
+                  </Alert>
+                </Stack>
+              )}
               <Grid item xs={12}>
                 <TextField
                   required
@@ -354,7 +479,7 @@ export default function UserRegistration() {
                   id="accountOwnerCustomer"
                   autoComplete="accountOwnerCustomer"
                   value={formData.accountOwnerCustomer}
-                  onChange={handleInputChange}
+                  onChange={handleFormdataInputChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -366,20 +491,10 @@ export default function UserRegistration() {
                   id="accountOwnerGW"
                   autoComplete="accountOwnerGW"
                   value={formData.accountOwnerGW}
-                  onChange={handleInputChange}
+                  onChange={handleFormdataInputChange}
                 />
               </Grid>
             </Grid>
-            {showError && (
-              <Stack
-                sx={{ display: "flex", justifyContent: "right" }}
-                spacing={2}
-              >
-                <Alert variant="filled" severity="error">
-                  Password Does Not Match
-                </Alert>
-              </Stack>
-            )}
             <Button
               type="submit"
               fullWidth

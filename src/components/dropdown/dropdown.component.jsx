c96d20596c95db76086ba0f036f60edf00b005f9
sreepriyaroy <sreepriya.roy@greenwave.co.in>
2024-03-23 19:25:54 +0530

    commit

--- src/components/dropdown/dropdown.component.jsx
+++ src/components/dropdown/dropdown.component.jsx
@@ -14,7 +14,7 @@ export default function Dropdown({
     <>
       <FormControl style={style} fullWidth={fullWidth}>
         <InputLabel>{label}</InputLabel>
-        <Select id={id} value={value} label={label} onChange={onChange}>
+        <Select id={id} value={value}  label={label} onChange={onChange}>
           {list.map((item, index) => (
             <MenuItem key={index} value={item}>
               {item}

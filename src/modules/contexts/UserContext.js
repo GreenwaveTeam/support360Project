03f64f20b512f47af2946164ef26dfa33f91cabc
trediptagw <tredipta.mondal@greenwave.co.in>
2024-04-03 14:44:36 +0530

    qwerty

--- src/modules/contexts/UserContext.js
+++ src/modules/contexts/UserContext.js
@@ -1,14 +1,35 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+// const UserContext = createContext();
+
+// export const UserProvider = ({ children }) => {
+//   const [userData, setUserData] = useState({
+//     userID: "",
+//     name: "",
+//     role: "",
+//     plantID: "",
+//   });
+
+//   return (
+//     <UserContext.Provider value={{ userData, setUserData }}>
+//       {children}
+//     </UserContext.Provider>
+//   );
+// };
+
+// export const useUserContext = () => useContext(UserContext);
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [userData, setUserData] = useState({
-    userID: "",
-    name: "",
-    role: "",
-    plantID: "",
+  const [userData, setUserData] = useState(() => {
+    const storedUserData = localStorage.getItem("userData");
+    return storedUserData ? JSON.parse(storedUserData) : {};
   });
+
+  useEffect(() => {
+    localStorage.setItem("userData", JSON.stringify(userData));
+  }, [userData]);
 
   return (
     <UserContext.Provider value={{ userData, setUserData }}>
